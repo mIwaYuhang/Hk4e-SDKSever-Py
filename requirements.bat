3ce904632b5530c1861bd510fd297febd77a806e
CokeSR <1500811684@qq.com>
2023-09-15 14:40:40 +0800

    First Upload Not Fix

---
+++ requirements.bat
@@ -0,0 +1,4 @@
+CHCP 65001
+pip install --no-cache-dir werkzeug==2.2.3 flask==2.2.4 requests rsa geoip2 bcrypt -i https://pypi.tuna.tsinghua.edu.cn/simple
+pause
+echo "Command：python main.py serve / initdb"
