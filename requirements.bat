8542df5a7005ed46b2dcd5057d23dcfff2e21818
CokeSR <1500811684@qq.com>
2023-09-15 14:46:02 +0800

    Fix Build-config

--- requirements.bat
+++ requirements.bat
@@ -1,4 +1,2 @@
-CHCP 65001
 pip install --no-cache-dir werkzeug==2.2.3 flask==2.2.4 requests rsa geoip2 bcrypt -i https://pypi.tuna.tsinghua.edu.cn/simple
-echo "Command：python main.py serve / initdb"
 pause
