0097c395e1aec4a3ee38d205bdd8ce9db70dfb10
GuoYiFan <2625053283@qq.com>
2023-09-18 22:15:29 +0800

    添加可动态开启的邮箱验证码注册功能

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask_mail import Mail
+from flask_caching import Cache
 from werkzeug.middleware.proxy_fix import ProxyFix
 app = Flask(__name__)
-
 import sys
 
 from config import load_config
@@ -10,13 +11,19 @@ import routes
 
 if __name__ == '__main__':
    config = load_config()
-
+   
    app.secret_key = config["app"]["secret_key"]
    app.debug = config["app"]["debug"]
    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
    if sys.argv[1] == "serve":
       print(">> Starting Flask server instance...")
+      mail_config = config.get('mail', {})
+      enable_mail = mail_config.get('open', False)
+
+      if enable_mail:
+         app.config.update(mail_config)
+         mail = Mail(app)
       app.run(config["app"]["listen"], config["app"]["port"])
    elif sys.argv[1] == "initdb":
       print(">> Initializing database structure...")
